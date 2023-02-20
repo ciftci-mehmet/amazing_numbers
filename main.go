55494948e93af11ad9e12345fc7f77c62f77aab0
ciftci-mehmet <mciftci@maritimetrainer.com>
2023-02-20 17:17:59 +0300

    refactor

--- main.go
+++ main.go
@@ -75,6 +75,7 @@ func printHelp() {
 			"   * the first parameter represents a starting number;\n" +
 			"   * the second parameter shows how many consecutive numbers are to be processed;\n" +
 			" - two natural numbers and a property to search for;\n" +
+			" - two natural numbers and two properties to search for;\n" +
 			" - separate the parameters with one space\n" +
 			" - enter 0 to exit."
 	fmt.Println(helpText)
@@ -92,26 +93,23 @@ func requestInput() string {
 
 func isInputsValid(inputs []string) (bool, string){
 	switch len(inputs) {
-	case 1:
-		if !isNaturalNumber(inputs[0]) {
-			return false, "The first parameter should be a natural number or zero."
+	case 4:
+		_, ok := propertiesMap[inputs[3]]
+		if !ok {
+			
+			message := fmt.Sprintf("The property [%s] is wrong.\n", inputs[3])
+			properties := ""
+			for k := range propertiesMap {
+				properties += k + ", "
+			}
+			properties = properties[:len(properties)-2]
+			message += fmt.Sprintf("Available properties: %v", properties)
+
+			return false, message
 		}
-	
-	case 2:
-		if !isNaturalNumber(inputs[0]) {
-			return false, "The first parameter should be a natural number or zero."
-		}
-		if !isNaturalNumber(inputs[1]) {
-			return false, "The second parameter should be a natural number."
-		}
+		fallthrough
 
 	case 3:
-		if !isNaturalNumber(inputs[0]) {
-			return false, "The first parameter should be a natural number or zero."
-		}
-		if !isNaturalNumber(inputs[1]) {
-			return false, "The second parameter should be a natural number."
-		}
 		_, ok := propertiesMap[inputs[2]]
 		if !ok {
 			
@@ -124,6 +122,18 @@ func isInputsValid(inputs []string) (bool, string){
 			message += fmt.Sprintf("Available properties: %v", properties)
 
 			return false, message
+		}
+		fallthrough
+		
+	case 2:
+		if !isNaturalNumber(inputs[1]) {
+			return false, "The second parameter should be a natural number."
+		}
+		fallthrough
+
+	case 1:
+		if !isNaturalNumber(inputs[0]) {
+			return false, "The first parameter should be a natural number or zero."
 		}
 	}
 	return true, ""
